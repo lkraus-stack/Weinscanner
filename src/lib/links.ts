624709ebe25d67f1e1eef7ac93e4256e76b0ebd5
Lukas Kraus <l.kraus@franco-consulting.com>
2026-05-17 06:24:43 +0200

    Update website links

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -1,6 +1,6 @@
 export const LINKS = {
-  privacy: 'https://www.franco-consulting.com/datenschutz',
-  imprint: 'https://www.franco-consulting.com/impressum',
-  support: 'https://www.franco-consulting.com/kontakt',
-  marketing: 'https://www.franco-consulting.com',
+  privacy: 'https://wein-scanner.app/datenschutz',
+  imprint: 'https://wein-scanner.app/impressum',
+  support: 'https://wein-scanner.app/support',
+  marketing: 'https://wein-scanner.app',
 } as const;
